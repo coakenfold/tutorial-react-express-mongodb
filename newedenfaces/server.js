c83d9d5e876f462288d1debbbe5326220794bfcb
Chad Oakenfold <web@oakenfold.ca>
2016-11-03 18:06:52 -0700

    POST /api/report

--- newedenfaces/server.js
+++ newedenfaces/server.js
@@ -315,6 +315,34 @@ app.get('/api/characters/:id', function(req, res, next) {
   });
 });
 
+/**
+* POST /api/report
+* Reports a character. Character is removed after 4 reports.
+*/
+app.post('/api/report', function(req, res, next) {
+  var characterId = req.body.characterId;
+
+  Character.findOne({ charterId: characterId }, function(err, character) {
+    if (err) return next(err);
+
+    if (!character) {
+      return res.status(404).send({ message: 'Character not found.' });
+    }
+
+    character.reports += 1;
+
+    if (character.reports > 4) {
+      character.remove();
+      return res.send({ message: character.name + ' has been deleted.'});
+    }
+
+    character.save(function(err) {
+      if (err) return next(err);
+
+      res.send({message: character.name + ' has been reported.'});
+    });
+  });
+});
 
 app.use(function(req, res){
   Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
